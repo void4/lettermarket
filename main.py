950a8dd29912045807a7e3e7ed751539a1332a10
void4 <arndtschnabel@freenet.de>
2021-02-14 00:30:45 +0100

    Add wordmarkets, show all auctions, even unattended ones

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ wordbanks = defaultdict(Counter)
 
 auctions = defaultdict(list)
 
+# only one sided, sell offers which the writer can accept?
+# or use buy offers as signal?
+wordmarkets = defaultdict(list)
+
+# in text box, allow greyed out words?
+
 characters = ascii_uppercase[:3]
 
 for char in characters:
@@ -50,49 +56,65 @@ def genword(letterbank):
 	# random legal word?
 
 	# if letterbank empty, return None
-	if not letterbank:
+	copied = deepcopy(letterbank)
+	totalletters = sum(copied.values())
+
+	if totalletters == 0:
 		return
 
 	word = ""
 
-	copied = deepcopy(letterbank)
-
-	for i in range(randint(1, sum(copied.values()))):
+	for i in range(randint(1, totalletters)):
 		randkey = choice(list(copied.keys()))
 		copied[randkey] -= 1
 		word += randkey
 
 	return word
 
+txlog = []
+
 def handle_message(message: twitch.chat.Message) -> None:
 	#    message.chat.send(f'@{message.user().display_name}, you have {message.user().view_count} views.')
 
-	#print(message.sender, message.text)
+	#print("MESSAGE", message.sender, message.text)
 
-	if random() < 0.1:
-		cmd = "!combine abc"
+	user = f"user{randint(0,4)}"
+
+	rand = random()
+
+	if rand < 0.1:
+		cmd = f"!combine {genword(letterbanks[user])}"
+	elif rand < 0.2:
+		writermoney = currencybank[None]
+		if writermoney < 3:
+			return
+		wordbank = wordbanks[user]
+		if len(wordbank.keys()) == 0:
+			return
+		cmd = f"!sell {choice(list(wordbank.keys()))} {randint(3,writermoney)}"
 	else:
 		cmd = f"!bid {choice(characters)} {randint(1,10)}"
 
 	#XXX user = message.sender
 
-	user = str(randint(0,9))
+
 
 	if not cmd.startswith("!"):
 		return
 
 	cmd = cmd[1:].split()
 
-
+	#print(cmd)
 
 	if cmd[0] == "combine" and len(cmd) >= 2:
-		word = genword(letterbanks[user])#XXXcmd[1]
+		word = cmd[1]
 		if word is None:
 			return
 
 		if subtract(letterbanks[user], word):
 			wordbanks[user][word] += 1
-			print(user, cmd)
+			txlog.append([user, cmd])
+
 	elif cmd[0] == "bid" and len(cmd) >= 3:
 		letter = cmd[1]
 		amount = int(cmd[2])#XXX ValueError
@@ -102,8 +124,27 @@ def handle_message(message: twitch.chat.Message) -> None:
 			return
 
 		auctions[letter].append([user, amount])
-		print(user, cmd)
+		txlog.append([user, cmd])
 
+	elif cmd[0] == "sell" and len(cmd) >= 3:
+		word = cmd[1]
+		amount = int(cmd[2])
+		# Also add timestamp to order
+		# Can't sell for more than writer posesses currently?
+		#if amount >= currencybank[None]
+
+		# can only sell words one owns
+		if wordbanks[user][word] == 0:
+			# TODO reply
+			return
+
+		# can only sell one at a time?
+		# Remove existing orders by this user in this market
+		wordmarkets[word] = [order for order in wordmarkets[word] if order[0] != user]
+
+		wordmarkets[word].append([user, amount])
+
+		txlog.append([user, cmd])
 
 tmi = twitch.tmi.TMI(config["client_id"], config["client_secret"])
 
@@ -130,27 +171,36 @@ while True:
 		# Settle auctions
 		print("Settling auctions...")
 
+		highestbids = {}
 		for letter, bids in auctions.items():
-			if len(bids) == 0:
-				# nobody bid on this letter
-				continue
 
 			# Sort bids by highest price first
 			bids = sorted(bids, key=lambda bid:bid[1], reverse=True)
 
 			# Have to search for highest bidder who can pay
 			# do alphabetically or randomly?
+
+			highestbid = None
+
 			for bid in bids:
 				bidder, amount = bid
 				if currencybank[bidder] >= amount:
 					currencybank[bidder] -= amount
+					highestbid = amount
 					letterbanks[bidder][letter] += 1
 					break
 
+			highestbids[letter] = highestbid
+
+		print(highestbids)
+
 		auctions = defaultdict(list)
+		for char in characters:
+			auctions[char]
 
 	if everysecond:
-		active = [chatter.name for chatter in tmi.chatters(config["channel"]).all()]
+		#XXX active = [chatter.name for chatter in tmi.chatters(config["channel"]).all()] + [None]
+		active = [f"user{i}" for i in range(5)] + [None]
 
 	now = time()
 
@@ -159,9 +209,7 @@ while True:
 	lastcheck = lastcheck + fullseconds
 
 	for user in active:
-		user = str(randint(0,9))#XXX
 		currencybank[user] += fullseconds
 
-	#print(bank)
-	if currencybank:
-		print(min(currencybank.values()), max(currencybank.values()), len(currencybank), len(active), Counter(currencybank.values()))
+	#if currencybank:
+	#	print(min(currencybank.values()), max(currencybank.values()), len(currencybank), len(active), Counter(currencybank.values()))
